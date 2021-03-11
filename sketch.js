3841da29a61a122d37c3646a8b0a532198d43fe8
HitenCoder <78018953+HitenCoder@users.noreply.github.com>
2021-03-11 08:37:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas;
+var gameState=0;
+var contestantCount;
+var database; 
+var quiz,question,contestant; 
+var allContestants;
+
+function setup(){
+  database = firebase.database();
+  canvas = createCanvas(850,400);
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+
+function draw(){
+  background("pink");
+
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
